c4c70695f822d3de3eda7902abc191a805cd0c71
Tuan Nguyen <tuannguyen08426@gmail.com>
2025-02-25 11:44:54 -0600

    Sidebar Implmentation
    - Designed sidebar with navigation compabilities with some limitations.
    - Includes basic icons and a button to collapse sidebar.
    
    Needs to implement profile icon and logout features.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,17 +2,6 @@ import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu"
-
-import Link from 'next/link'
-
-
 const robotoMono = Roboto_Mono({
   variable : "--font-roboto-mono",
   subsets : ["latin"]
@@ -33,43 +22,6 @@ export default function RootLayout({
       <body
         className={`${robotoMono.variable} antialiased`}
       >
-        <div className="flex justify-end py-2 px-4">
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Home
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/taskview" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Task Views
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/help" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Help
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/preferences" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Preferences
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                Logout
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
         {children}
       </body>
     </html>
