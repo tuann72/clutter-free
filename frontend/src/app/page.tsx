4a785494ff66fab0e6efc383dc37de556040f45a
Tuan Nguyen <tuannguyen08426@gmail.com>
2025-02-27 14:54:04 -0600

    Changed the color and text on the root directory.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,15 @@ import { SignIn, useUser } from '@clerk/nextjs'
 export default function Login() {
   return (
     <div className="grid grid-cols-[1.5fr_1fr] h-screen">
-      <div className="bg-blue-300">
-        Column 1
+      <div className="flex justify-center items-center bg-slate-200">
+        <div className='flex-col'>
+          <p className='text-5xl'>
+          ClutterFree
+          </p>
+          <span>
+            LOGIN CURRENTLY NOT WORKING
+          </span>
+        </div>
       </div>
       <div className="place-self-center">
         <SignIn />
