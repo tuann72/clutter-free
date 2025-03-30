fdafd6f958b3e9067fc39ab08dde238766691edd
Tuan Nguyen <tuannguyen08426@gmail.com>
2025-03-29 21:02:45 -0500

    added more boiler plate code for taskview page

--- frontend/src/app/(app)/taskview/page.tsx
+++ frontend/src/app/(app)/taskview/page.tsx
@@ -1,10 +1,35 @@
+'use client'
+import { TaskViewComboBox } from "@/components/taskviewComboBox"
+import { GraphViewComboBox } from "@/components/graphComboBox"
 import {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup,
   } from "@/components/ui/resizable"
 
+import { ListView } from "@/components/listView"
+import { useState } from "react"
+
+const viewOptions = [
+  {
+    value: "list_view",
+    label: "List",
+  },
+  {
+    value: "kanban",
+    label: "Kanban",
+  },
+  {
+    value: "mindmap",
+    label: "Mindmap",
+  },
+]
+
 export default function TaskView(){
+
+    const [view, setView] = useState("list_view")
+    const [graph, setGraph] = useState("bar")
+
     return(
         <ResizablePanelGroup
           direction="horizontal"
@@ -19,16 +44,25 @@ export default function TaskView(){
                 </ResizablePanel>
                 <ResizableHandle withHandle/>
                 <ResizablePanel defaultSize={50}>
-                    <div className="flex h-full items-center justify-center p-6">
-                    <span className="font-semibold">Filter Section</span>
+                    <div className="flex flex-col h-full items-center p-6">
+                    <div className="font-semibold pb-4">Filter Section</div>
+                      <div className="flex flex-col lg:flex-row gap-2">
+                        <GraphViewComboBox value={graph} onValueChange={setGraph} />
+                        <TaskViewComboBox value={view} onValueChange={setView} />
+                      </div>
                     </div>
                 </ResizablePanel>
             </ResizablePanelGroup>
           </ResizablePanel>
           <ResizableHandle withHandle />
           <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Content Section</span>
+            <div className="flex flex-col h-full items-center justify-center p-6">
+              <div>
+                {viewOptions.find((option) => option.value === view)?.label || "Select a view!"}
+              </div>
+              {
+                view === "list_view" && <ListView />
+              }
             </div>
           </ResizablePanel>
         </ResizablePanelGroup>
---
+++ frontend/src/components/taskviewComboBox.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+const options = [
+  {
+    value: "list_view",
+    label: "List",
+  },
+  {
+    value: "kanban",
+    label: "Kanban",
+  },
+  {
+    value: "mindmap",
+    label: "Mindmap",
+  },
+]
+
+export function TaskViewComboBox({
+  value,
+  onValueChange,
+}: {
+  value: string
+  onValueChange: (value: string) => void
+}) {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : "Select Style"}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandGroup>
+              {options.map((option) => (
+                <CommandItem
+                  key={option.value}
+                  value={option.value}
+                  onSelect={(currentValue) => {
+                    setOpen(false)
+                    onValueChange(currentValue === value ? "" : currentValue)
+                  }}
+                >
+                  {option.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === option.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
