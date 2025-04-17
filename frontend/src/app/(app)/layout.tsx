5f8534156a4929424d5cf8c45b484a21e3d335bb
Mahnoor Saeed <mahnoorsaeed83@gmail.com>
2025-04-17 18:57:41 -0500

    theme, shortcuts, and font

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -1,10 +1,14 @@
-'use client'
+'use client';
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Eye, EyeClosed } from "lucide-react";
 import { useUserInfo } from '@/context/UserContext';
+import { ThemeProvider } from "@/components/theme-provider";
+import KeyboardShortcutHandler from "@/components/keyboard-shortcut-handler";
+import FontScaleGlobal from "@/components/FontSizeGlobal";
+import FontTypeHandler from "@/components/FontTypeHandler";
 
 export default function Layout({children} : {children: React.ReactNode}){
 
@@ -17,9 +21,13 @@ export default function Layout({children} : {children: React.ReactNode}){
     const userInfo = useUserInfo();
 
     return(
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <SidebarProvider>
             <AppSidebar />                
             <SidebarTrigger />
+            <KeyboardShortcutHandler />
+        <FontScaleGlobal />
+        <FontTypeHandler />
             <div className="flex absolute top-0 right-0 px-2 gap-2">
                 <Button variant='ghost' size='icon' onClick={toggleVisibility}>
                     {toggleTopRightOptions === false && <Eye/>}
@@ -41,5 +49,6 @@ export default function Layout({children} : {children: React.ReactNode}){
             </div>
             {children}
         </SidebarProvider>
+        </ThemeProvider>
     )
 }
